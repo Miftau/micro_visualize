26d8524fb474ceb5e3f92e560d2f0d1e7baa28fc
Miftau <miftaudeenb@gmail.com>
2025-08-11 01:53:17 +0100

    adding manual cron job

--- app.py
+++ app.py
@@ -4,13 +4,31 @@ from flask_wtf.csrf import CSRFProtect
 import os
 from wtforms.widgets import html_params
 from markupsafe import Markup
-
+import requests
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "dev_secret_key")
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'Uploads')
 from server.routes import bp
 app.register_blueprint(bp)
+
+PING_URL = "https://microanalyze.onrender.com"
+def keep_alive():
+    try:
+        response = requests.get(PING_URL)
+        print(f"[Keep Alive] Status: {response.status_code}")
+    except Exception as e:
+        print(f"[Keep Alive] Failed: {e}")
+
+# Schedule the job every 10 minutes
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=keep_alive, trigger="interval", minutes=10)
+scheduler.start()
+
+# Shut down scheduler when exiting the app
+import atexit
+atexit.register(lambda: scheduler.shutdown())
 
 def render_field(field, **kwargs):
     return Markup(f"""
